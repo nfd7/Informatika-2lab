4b479f1be1c82fe265127b3ba0e75d2eb1e211bf
NFD <fin.makmsitr@mail.ru>
2020-11-04 18:50:52 +0300

    2commit

--- source/repos/second lab/laba2.c
+++ source/repos/second lab/laba2.c
@@ -1,28 +1,20 @@
-#include <stdio.h>
-#define N 255
+#include <stdio.h> 
 int main()
 {
-    int i, b, j = 0, c;
-    char str[N];
-    printf("Enter string: \n");
-    fgets(str, N, stdin);
-    printf("Enter letter: \n");
-    c = getc(stdin);
-    for (i = 0; str[i]; ++i)
+    char c, s[200];
+    char* b, * e;
+    printf("enter the letter : ");
+    c = getch();
+    printf("%c\nenter the string : ", c);
+    gets(s);
+    b = s;
+    while (e = strstr(b, " "))
     {
-        if (str[i] == c)
-        {
-            if (i == 0 || ispunct(str[i - 1]) || isblank(str[i - 1]))
-            {
-                b = i;
-                while (str[b] != '\n' && !ispunct(str[b]) && !isblank(str[b]))
-                    ++b;
-                for (j = i; j != b; ++j)
-                    printf("%c", str[j]);
-                putchar(' ');
-            }
-            i = j;
-        }
+        *e = 0;
+        if (*b == c) 
+        printf("%s\n", b);
+        b = e + 1;
     }
-    return 0;
+    if (*b == c)
+    printf("%s\n", b);
 }
