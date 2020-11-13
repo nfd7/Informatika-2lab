9ffcfbc6b75539e278bc94384dc8ad03adf3b56f
NFD <fin.makmsitr@mail.ru>
2020-11-13 17:17:58 +0300

    laba2

--- source/repos/second lab/laba2.c
+++ source/repos/second lab/laba2.c
@@ -3,18 +3,23 @@ int main()
 {
     char c, s[200];
     char* b, * e;
-    printf("enter the letter : ");
-    c = getch();
-    printf("%c\nenter the string : ", c);
-    gets(s);
-    b = s;
-    while (e = strstr(b, " "))
-    {
-        *e = 0;
-        if (*b == c)
-        printf("Words beginning with letter '%c' %s\n", c, b);
-        b = e + 1;
-    }
-    if (*b == c)
-    printf("Words beginning with letter '%c' %s\n", c, b);
+        printf("enter the letter : ");
+        c = getch();
+        if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
+        {
+            printf("%c\nenter the string : ", c);
+            gets(s);
+            b = s;
+            while (e = strstr(b, " "))
+            {
+                    *e = 0;
+                    if (*b == c)
+                        printf("Words beginning with letter '%c' %s\n", c, b);
+                    b = e + 1;
+                    if (*b == c)
+                        printf("Words beginning with letter '%c' %s\n", c, b);
+            }
+        }
+        else printf(" \n '%c' is not a letter", c);
+        return 0;
 }
